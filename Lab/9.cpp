7989dc60caf09bd6aa95e4b10b3b3cd997c987e7
jungbinl <jbiny1004@naver.com>
2025-10-26 22:56:04 -0700

    Create 9.cpp

---
+++ Lab/9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+// Added Code Here
+using namespace std;
+
+// REFERENCE ON HOW TO USE ARRAY OF OBJECTS : https://www.geeksforgeeks.org/cpp/array-of-objects-in-c-with-examples/
+
+int main()
+{
+    int x, y, n;
+    cout << "Enter Number of Points: " << endl;
+    // Added Code Here. Name of arrray of objects is points
+    // COUT Statements to be used:
+    cout << endl
+         << "Distance from origin: " << fixed << setprecision(3) << points[i].dist_from_origin() << endl;
+
+    int p1, p2;
+    cout << "Enter two points to find distance between them: " << endl;
+    // Added Code Here
+
+    // COUT Statements to be used:
+    cout << "Distance between points " << fixed << setprecision(3) << p1 << " and " << p2 << " is " << /* Added Code Here <<*/;
+    return 0;
+}
