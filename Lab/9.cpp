cdf4d92dc7c3cbd8e9e7ad4b1e2250b23a38f715
jungbinl <jbiny1004@naver.com>
2025-10-26 22:57:04 -0700

    Implement point input and distance calculations
    Added functionality to input points and calculate distances.

--- Lab/9.cpp
+++ Lab/9.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-// Added Code Here
+#include "Lab9.h"
 using namespace std;
 
 // REFERENCE ON HOW TO USE ARRAY OF OBJECTS : https://www.geeksforgeeks.org/cpp/array-of-objects-in-c-with-examples/
@@ -10,16 +10,23 @@ int main()
 {
     int x, y, n;
     cout << "Enter Number of Points: " << endl;
-    // Added Code Here. Name of arrray of objects is points
-    // COUT Statements to be used:
+    cin >> n;
+    Point points[n];
+    for (int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        points[i].set_points(a, b);
+        points[i].print();
     cout << endl
          << "Distance from origin: " << fixed << setprecision(3) << points[i].dist_from_origin() << endl;
-
+    }
     int p1, p2;
     cout << "Enter two points to find distance between them: " << endl;
-    // Added Code Here
-
-    // COUT Statements to be used:
-    cout << "Distance between points " << fixed << setprecision(3) << p1 << " and " << p2 << " is " << /* Added Code Here <<*/;
+     cin >> p1 >> p2;
+     if(p1 > n-1 || p2 > n-1 || p1 < 0 || p2 < 0){
+        cout << "Invalid Input" << endl;
+     } else{
+          cout << "Distance between points " << fixed << setprecision(3) << p1 << " and " << p2 << " is " << points[p1].dist(points[p2]);
+     }
     return 0;
 }
