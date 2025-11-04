5620664d19bf613adb036b93d83d64864828bfd4
jungbinl <jbiny1004@naver.com>
2025-11-04 12:23:23 -0700

    Add Lab3_2.cpp to calculate HCF

---
+++ Lab/Lab3/Lab3_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+  // Complete the code the below where indicated '/*Code Here*/'.
+  // DO NOT CHANGE THE VARIABLE NAMES
+    int a= 12;
+    int b= 18;
+    int L= 36;
+    int HCF= (a*b)/L;;
+    cout << "HCF is" << HCF;
+   }
